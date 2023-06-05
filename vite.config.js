eab3c5f898f5a647b0ed6e80fa1371988840960b
nathan29849 <phs960418@gmail.com>
2023-06-06 02:06:54 +0500

    configs added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    devSourcemap: true,
+  },
   plugins: [react()],
-})
+});
